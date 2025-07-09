4e588b72aa9d143c4a8024cf5eaab7711a3ca853
Louis Ouellet <louis_ouellet@hotmail.com>
2025-07-09 15:47:47 -0400

    General: Code Standardization & Clean-up

--- Model.php
+++ Model.php
@@ -1,132 +1,167 @@
 <?php
 
-/**
- * Core Framework - FollowupsModel
- *
- * @license    MIT (https://mit-license.org/)
- * @author     Louis Ouellet <louis@laswitchtech.com>
- */
-
 // Import additionnal class into the global namespace
-use \LaswitchTech\Core\Abstracts\Model;
+use \LaswitchTech\Core\Base\BaseModel;
 
-class FollowupsModel extends Model {
+class FollowupsModel extends BaseModel {
 
     /**
-     * Create a new followup and return the id
-     *
-     * @param array $data
-     * @return int
+     * Constructor
      */
-    public function create(array $data): int
+    public function __construct()
     {
-        // Create the Query
-        $Query = $this->Database->query()
-            ->table('followups')
-            ->insert($data);
+        // Call the parent constructor
+        parent::__construct();
 
-        // Execute the Query
-        $affectedRows = $Query->execute();
-
-        // Execute the Query
-        return $Query->lastId();
+        // Initialize the Model
+        $this->init('followups');
     }
 
     /**
-     * Update a followup
+     * Process a record
      *
-     * @param int $id
-     * @param array $data
-     * @return int
-     */
-    public function update(int $id, array $data): int
-    {
-        // Create the Query
-        $Query = $this->Database->query()
-            ->table('followups')
-            ->update($data)
-            ->where('id', $id);
-
-        // Execute the Query
-        return $Query->execute();
-    }
-
-    /**
-     * Retrieve Followups List
-     *
-     * @param int $organization
+     * @param array $record
      * @return array
      */
-    public function list(int $organization): array
+    protected function process(array $record): array
+    {
+        // Call the parent constructor
+        $record = parent::process($record);
+
+        // Check if the record has a task
+        if(array_key_exists('task', $record) && array_key_exists('process', $record['task'])){
+
+            // Check if the process is a valid JSON string
+            if(is_string($record['task']['process']) && $this->isJson($record['task']['process'])){
+
+                // Decode the JSON value
+                $record['task']['process'] = json_decode($record['task']['process'], true);
+            }
+        }
+
+        // Check if the vcard exists in the record
+        if(array_key_exists('vcard', $record)){
+
+            // Check if the vcard has tags
+            if(array_key_exists('tags', $record['vcard'])){
+
+                // Check if the process is a valid JSON string
+                if(is_string($record['vcard']['tags']) && $this->isJson($record['vcard']['tags'])){
+
+                    // Decode the JSON value
+                    $record['vcard']['tags'] = json_decode($record['vcard']['tags'], true);
+                }
+            }
+
+            // Check if the vcard has industries
+            if(array_key_exists('industries', $record['vcard'])){
+
+                // Check if the process is a valid JSON string
+                if(is_string($record['vcard']['industries']) && $this->isJson($record['vcard']['industries'])){
+
+                    // Decode the JSON value
+                    $record['vcard']['industries'] = json_decode($record['vcard']['industries'], true);
+                }
+            }
+        }
+
+        // Return the processed record
+        return $record;
+    }
+
+    /**
+     * Retrieve multiple records
+     *
+     * @param array $conditions
+     * @return array
+     */
+    public function fetchAll(array $conditions = [], string $conjunction = 'AND'): array
     {
         // Create the Query
         $Query = $this->Database->query()
-            ->table('followups')
+            ->table($this->table)
             ->select('*')
             ->join('owner', 'users', 'username')
             ->join('assignedTo', 'users', 'id')
-            ->join('task', 'tasks', 'id')
             ->join('vcard', 'vcards', 'id')
+            ->join('task', 'tasks', 'id')
             ->join('organization', 'organizations', 'id')
             ->filter()
             ->where('id', 9999, '<>')
-            ->where('isArchived', 0)
-            ->filter()
-            ->where('organization', $organization);
+            ->where('organization', $this->Auth->user()->organization()->id);
+
+        // Check if the conditions are empty
+        if(!empty($conditions)){
+
+            // Add a Filter
+            $Query->filter();
+
+            // Add the Conditions
+            foreach($conditions as $key => $condition){
+
+                // Check if the key exists in the definition
+                if(!array_key_exists($condition['key'], $this->definition)){
+
+                    // Remove the key from the data
+                    unset($conditions[$key]);
+                    continue;
+                }
+
+                // Add the condition to the Query
+                $Query->where($condition["key"], $condition["value"], $condition["operator"], $conjunction);
+            }
+        }
 
         // Retrieve the Results
-        $result = $Query->result();
+        $records = $Query->fetch();
 
-        // Process Records
-        foreach($result as $key => $record){
+        // Loop through the records to process them
+        foreach($records as $key => $record){
 
-            // Decode JSON Fields
-            $result[$key]['task']['process'] = json_decode($record['task']['process'] ?? '[]', true);
-            $result[$key]['vcard']['tags'] = json_decode($record['vcard']['tags'] ?? '[]', true);
-            $result[$key]['vcard']['industries'] = json_decode($record['vcard']['industries'] ?? '[]', true);
+            // Overwrite the record with the processed one
+            $records[$key] = $this->process($record);
         }
 
         // Return the Results
-        return $result;
+        return $records;
     }
 
     /**
-     * Retrieve Followup's Details
+     * Retrieve a single record
      *
      * @param int $id
      * @return array
      */
-    public function get(int $id): array
+    public function fetch(int $id): array
     {
         // Create the Query
         $Query = $this->Database->query()
-            ->table('followups')
+            ->table($this->table)
             ->select('*')
             ->join('owner', 'users', 'username')
             ->join('assignedTo', 'users', 'id')
-            ->join('task', 'tasks', 'id')
+            ->join('assignedTo.vcard', 'vcards', 'id')
             ->join('vcard', 'vcards', 'id')
+            ->join('task', 'tasks', 'id')
             ->join('organization', 'organizations', 'id')
+            ->join('organization.vcard', 'vcards', 'id')
             ->filter()
             ->where('id', 9999, '<>')
-            ->where('isArchived', 0)
             ->filter()
-            ->where('id', $id)
+            ->where($this->primary, $id)
             ->limit(1);
 
-        // Retrieve the Results
-        $result = $Query->result();
+        // Retrieve the record
+        $records = $Query->fetch();
 
-        // Process Records
-        foreach($result as $key => $record){
+        // Loop through the records to process them
+        foreach($records as $key => $record){
 
-            // Decode JSON Fields
-            $result[$key]['task']['process'] = json_decode($record['task']['process'] ?? '[]', true);
-            $result[$key]['vcard']['tags'] = json_decode($record['vcard']['tags'] ?? '[]', true);
-            $result[$key]['vcard']['industries'] = json_decode($record['vcard']['industries'] ?? '[]', true);
+            // Overwrite the record with the processed one
+            $records[$key] = $this->process($record);
         }
 
-        // Return the Results
-        return $result[array_key_first($result)] ?? [];
+        // Return the record or an empty array if not found
+        return $records[array_key_first($records)] ?? [];
     }
 }
